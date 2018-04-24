751cbeaafe49de74f987f3a18e089519d22e4341
Your Name <you@example.com>
2018-04-24 13:45:16 +0200

    Reduced split size to 800.000 Reduced file name lenght

--- 02_split/run_job.sh
+++ 02_split/run_job.sh
@@ -4,7 +4,7 @@ echo '#'
 echo '#  Starting Job: Split'
 echo '#'
 
-split_file_suffix="${work_directory}/${file_name_no_ext}/${file_name_no_ext}-split"
+split_file_suffix="${work_directory}/${file_name_no_ext}/${file_name_no_ext}-"
 # Debug: Show Paths
 echo "!! work_path", ${work_path}
 echo "!! extract_directory", ${extract_directory}
@@ -16,7 +16,7 @@ echo "!! split_file_suffix", ${split_file_suffix}
 mkdir ${extract_directory}
 
 echo "   Split file int work directory"
-split -l 1000000 --additional-suffix=.csv ${work_path}  ${split_file_suffix}
+split -l 800000 --additional-suffix=.csv ${work_path}  ${split_file_suffix}
 
 # Run move_to_output as a subprocess passing all variables
 source /code/move_to_output.sh
