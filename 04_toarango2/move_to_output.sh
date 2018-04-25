b48de99b850fdfc1ad4ff1128451999bbf9b2988
Steffen Dalgard <steffen.dalgard@sintef.no>
2018-04-25 11:17:51 +0200

    New version of to arango with faster input lock using rename and no output lock

---
+++ 04_toarango2/move_to_output.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo '#'
+echo '#  Starting move_to_output:'
+echo '#'
+# Debug: Show Paths
+echo "!! work_path_results", ${work_path_results}
+echo "!! output_directory", ${output_directory}
+
+# Files are now ready processed ... ready to be moved
+
+output_lock_file="${output_directory}/dir_rw.lock"
+
+
+# # Aquire lock
+# exec 9>$output_lock_file
+# echo "// Aquire lock ${output_lock_file}"
+# if flock 9; then
+      
+    # Move to Output, the lock will assure complete operation before starting to consume
+    echo "   Moving From work To Output without lock"
+    mv ${work_path_results}/${file_name_no_ext}* ${output_directory}
+
+# fi
+# # Release the lock
+# exec 9>&-
+
---
+++ 04_toarango2/run_job.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo '#'
+echo '#  Starting Job: Toarango'
+echo '#'
+# Debug: Show Paths
+echo "!! work_path", ${work_path}
+echo "!! transformation_json_full_path", ${transformation_json_full_path}
+
+# Files are now in the work dir ... ready to be processed
+
+# Transforming
+echo "   Transforming to Arango Graph"
+cd ${work_directory}
+node \
+    /code/Datagraft-RDF-to-Arango-DB/transformscript.js \
+    -t ${transformation_json_full_path} \
+    -f ${work_path}
+
+ls -l ${work_directory}
+ls -l ${work_path_results}
+
+# Run move_to_output as a subprocess passing all variables
+source /code/move_to_output.sh
+
+echo '   Done'
+
